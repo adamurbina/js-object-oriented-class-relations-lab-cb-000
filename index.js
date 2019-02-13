d3b2e332566c7ff264ee88a9003a786c8f798d3f
Adam Urbina <adamurbina@gmail.com>
2019-02-13 14:11:26 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,61 @@
+
+let driverId = 0;
+const store = { drivers:[], passengers:[], trips:[] }
+
+class Driver{
+  constructor(name){
+    this.id = ++driverId;
+    this.name = name;
+    store.drivers.push(this);
+  };
+
+  trips(){
+    return store.trips.filter(
+      function (trip) {
+        return trip.driverId === this.id;
+      }.bind(this)
+    );
+  };
+}
+
+let passengerId = 0;
+
+class Passenger{
+  constructor(name){
+    this.id = ++passengerId;
+    this.name = name;
+    store.passengers.push(this);
+  };
+}
+
+let tripId = 0;
+
+class Trip{
+  constructor(driver, passenger){
+    this.id = ++ tripId;
+    store.trips.push(this);
+    if (driver){
+      this.driverId = driver.id;
+    };
+    if (passenger){
+      this.passengerId = passenger.id;
+    };
+  };
+
+  driver(){
+    return store.drivers.find(
+      function(driver){
+        return driver.id === this.driverId;
+      }.bind(this)
+    );
+  };
+
+  passenger(){
+    return store.passengers.find(
+      function(passenger){
+        return passenger.id === this.passengerId;
+      }.bind(this)
+    );
+  };
+
+}
